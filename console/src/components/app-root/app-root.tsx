9801989fd05db889765df9275a735877245f24f5
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-02-19 09:55:33 +0900

    chore: render cache at app-root

--- console/src/components/app-root/app-root.tsx
+++ console/src/components/app-root/app-root.tsx
@@ -1,4 +1,4 @@
-import { Component, Fragment, h, Host } from '@stencil/core';
+import { Component, h, Host } from '@stencil/core';
 import { AppEnv } from 'announsing-shared';
 import { App } from 'src/app/app';
 import { AppFirebase } from 'src/app/firebase';
@@ -47,11 +47,47 @@ export class AppRoot {
     */
   }
 
-  private renderRoute(Tag: string, path: RoutePath) {
-    return (
-      // eslint-disable-next-line react/jsx-no-bind
-      <Route path={path} render={params => <Tag app={this.app} class="page" {...params}></Tag>} />
+  private _renderItems: { signedIn: any; notSignedIn: any; footer: any };
+  private get renderItems() {
+    if (this._renderItems) {
+      return this._renderItems;
+    }
+    const renderRoute = (Tag: string, path: RoutePath) => {
+      return (
+        // eslint-disable-next-line react/jsx-no-bind
+        <Route path={path} render={params => <Tag app={this.app} class="page" {...params}></Tag>} />
+      );
+    };
+
+    const signedIn = [
+      renderRoute('app-home', '/'),
+      renderRoute('app-announce-create', '/a/create'),
+      renderRoute('app-announce-edit', match('/a/:announceID/edit')),
+      renderRoute('app-posts', match('/a/:announceID/p')),
+      <Route path="/signin" to="/"></Route>,
+    ];
+
+    const notSignedIn = [
+      renderRoute('app-signin', '/signin'),
+      <Route path={/.*/} to="/signin"></Route>,
+    ];
+
+    const footer = (
+      <footer>
+        <div class="title">{this.app.msgs.footer.title}</div>
+        <div class="copy">&copy;suzulabo</div>
+        <div class="build-info">Version: {this.app.buildInfo.src}</div>
+        <div class="build-info">Built at {new Date(this.app.buildInfo.time).toISOString()}</div>
+        <div class="github">
+          <a href="https://github.com/suzulabo/announsing">
+            <ap-icon icon="github" />
+          </a>
+        </div>
+      </footer>
     );
+
+    this._renderItems = { signedIn, notSignedIn, footer };
+    return this._renderItems;
   }
 
   render() {
@@ -60,33 +96,10 @@ export class AppRoot {
     return (
       <Host>
         <Router.Switch>
-          {signIn && (
-            <Fragment>
-              {this.renderRoute('app-home', '/')}
-              {this.renderRoute('app-announce-create', '/a/create')}
-              {this.renderRoute('app-announce-edit', match('/a/:announceID/edit'))}
-              {this.renderRoute('app-posts', match('/a/:announceID/p'))}
-              <Route path="/signin" to="/"></Route>
-            </Fragment>
-          )}
-          {!signIn && (
-            <Fragment>
-              {this.renderRoute('app-signin', '/signin')}
-              <Route path={/.*/} to="/signin"></Route>
-            </Fragment>
-          )}
+          {signIn && this.renderItems.signedIn}
+          {!signIn && this.renderItems.notSignedIn}
         </Router.Switch>
-        <footer>
-          <div class="title">{this.app.msgs.footer.title}</div>
-          <div class="copy">&copy;suzulabo</div>
-          <div class="build-info">Version: {this.app.buildInfo.src}</div>
-          <div class="build-info">Built at {new Date(this.app.buildInfo.time).toISOString()}</div>
-          <div class="github">
-            <a href="https://github.com/suzulabo/announsing">
-              <ap-icon icon="github" />
-            </a>
-          </div>
-        </footer>
+        {this.renderItems.footer}
         <ap-loading class={{ show: this.app.loading }} />
       </Host>
     );
