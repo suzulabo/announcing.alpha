4b5a5ec77869b4943231527da31922d948ac9dd6
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-02-22 16:00:29 +0900

    refactor: get image function

--- firebase/functions/src/get-data.ts
+++ firebase/functions/src/get-data.ts
@@ -2,25 +2,21 @@ import * as admin from 'firebase-admin';
 import { Request, Response } from 'firebase-functions';
 import { converters } from './firestore';
 
-export const httpsGetData = async (req: Request, res: Response, adminApp: admin.app.App) => {
-  const d = await getData(req.path, adminApp);
-  if (typeof d == 'number') {
-    res.sendStatus(d);
+const cacheControl = 'public, max-age=31556952, s-maxage=86400, immutable';
+const announcePattern = /^\/dt\/a\/([0-9A-Z]{12})\/(m|p)\/([0-9A-Za-z]{1,8})$/;
+const imagePattern = /^\/dt\/i\/([0-9A-Za-z]{22})$/;
+
+export const httpsGetAnnounceData = async (
+  req: Request,
+  res: Response,
+  adminApp: admin.app.App,
+) => {
+  console.log(req.path);
+  const m = announcePattern.exec(req.path);
+  if (!m) {
+    res.sendStatus(400);
     return;
   }
-
-  res.setHeader('Cache-Control', 'public, max-age=31556952, s-maxage=86400, immutable');
-  res.json(d);
-};
-
-const pathPattern = /^\/dt\/a\/([0-9A-Z]{12})\/(m|p)\/([0-9A-Za-z.-]{8})$/;
-
-const getData = async (p: string, adminApp: admin.app.App) => {
-  const m = pathPattern.exec(p);
-  if (!m) {
-    return 400;
-  }
-
   const [, id, _sub, subId] = m;
   const sub = _sub == 'm' ? 'meta' : 'posts';
   const converter: admin.firestore.FirestoreDataConverter<unknown> =
@@ -30,8 +26,34 @@ const getData = async (p: string, adminApp: admin.app.App) => {
   const docRef = firestore.doc(`announces/${id}/${sub}/${subId}`).withConverter(converter);
   const data = (await docRef.get()).data();
   if (!data) {
-    return 404;
+    res.sendStatus(404);
+    return;
   }
 
-  return data;
+  res.setHeader('Cache-Control', cacheControl);
+  res.json(data);
+};
+
+export const httpsGetImageData = async (req: Request, res: Response, adminApp: admin.app.App) => {
+  const m = imagePattern.exec(req.path);
+  if (!m) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const [, id] = m;
+
+  const firestore = admin.firestore();
+  const docRef = firestore.doc(`images/${id}`);
+  const data = (await docRef.get()).data();
+  if (!data) {
+    res.sendStatus(404);
+    return;
+  }
+
+  const img = data.d as Buffer;
+
+  res.setHeader('Cache-Control', cacheControl);
+  res.setHeader('Content-Type', 'image/jpeg');
+  res.send(img).end();
 };
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,7 +4,7 @@ import * as functions from 'firebase-functions';
 import { callCreateAnnounce } from './create-announce';
 import { callDeleteAnnounce, firestoreDeleteAnnounce } from './delete-announce';
 import { callEditAnnounce } from './edit-announce';
-import { httpsGetData } from './get-data';
+import { httpsGetAnnounceData, httpsGetImageData } from './get-data';
 import { callPutPost } from './put-post';
 
 const adminApp = initializeApp();
@@ -25,9 +25,18 @@ export const putPost = region.https.onCall(async (data, context) => {
   return callPutPost(data, context, adminApp);
 });
 
-export const getData = region.https.onRequest(async (req, res) => {
+export const getAnnounceData = region.https.onRequest(async (req, res) => {
   try {
-    await httpsGetData(req, res, adminApp);
+    await httpsGetAnnounceData(req, res, adminApp);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+export const getImageData = region.https.onRequest(async (req, res) => {
+  try {
+    await httpsGetImageData(req, res, adminApp);
   } catch (err) {
     console.error(err);
     res.sendStatus(500);
