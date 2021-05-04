0dbd392e32e3ead326e11046e2498c46965d1b17
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-05-04 13:26:09 +0900

    chore: test codes moved (functions

---
+++ firebase/functions/__test__/utils/firestore.test.ts
@@ -0,0 +1,14 @@
+import { AnnounceMeta } from '../../src/shared';
+import { announceMetaHash, _serialize } from '../../src/utils/firestore';
+
+describe('firestore', () => {
+  it('_serialize', () => {
+    const a = _serialize('a', 'b', 'c');
+    expect(a).toEqual(_serialize('a', 'b', 'c', undefined, undefined));
+  });
+  it('announceMetaHash', () => {
+    const a = announceMetaHash({ name: 'a' } as AnnounceMeta);
+    expect(a).not.toEqual(announceMetaHash({ name: 'b' } as AnnounceMeta));
+    expect(a).toEqual(announceMetaHash({ name: 'a', link: '' } as AnnounceMeta));
+  });
+});
