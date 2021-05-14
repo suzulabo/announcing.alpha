200ccdfe9074cf581052337ddc811a8eb163fa1d
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-05-14 14:57:54 +0900

    chore: set firestore indexes

---
+++ firebase/firestore-indexes-gen.js
@@ -0,0 +1,30 @@
+const disables = {
+  'announces': ['mid', 'posts', 'uT'],
+  'meta': ['name', 'desc', 'link', 'icon', 'cT'],
+  'posts': ['title', 'body', 'link', 'img', 'pT'],
+  'images': ['data'],
+  'notif-devices': ['signKey', 'lang', 'tz', 'follows', 'uT'],
+  'notif-imm': ['announceID', 'followers', 'unfollows', 'archives', 'uT'],
+  'notif-timed': ['time', 'followers', 'unfollows', 'archives', 'uT'],
+  'archives': ['followers'],
+};
+
+const fieldOverrides = Object.entries(disables).reduce((a, [collectionGroup, fields]) => {
+  a.push(
+    ...fields.map(fieldPath => {
+      return {
+        collectionGroup,
+        fieldPath,
+        indexes: [],
+      };
+    }),
+  );
+  return a;
+}, []);
+
+const json = {
+  indexes: [],
+  fieldOverrides,
+};
+
+process.stdout.write(JSON.stringify(json, null, 2));
