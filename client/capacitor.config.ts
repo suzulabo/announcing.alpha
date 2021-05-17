e00b2f05c512df7aeb8ef3a47ce5495bc8250c65
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-05-17 18:16:45 +0900

    feat: Add android app packages

---
+++ client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.announcing.client',
+  appName: 'Announcing♪',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
