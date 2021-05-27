65a4fb213143ccd4d404aaf4996d90da0cd95dbb
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-05-28 04:16:49 +0900

    feat: delayed show (ap-spinner

--- shared-ui/src/ap-spinner/ap-spinner.tsx
+++ shared-ui/src/ap-spinner/ap-spinner.tsx
@@ -1,14 +1,23 @@
-import { Component, h, Host } from '@stencil/core';
+import { Component, h, Host, State } from '@stencil/core';
 
 @Component({
   tag: 'ap-spinner',
   styleUrl: 'ap-spinner.scss',
 })
 export class ApSpinner {
+  @State()
+  visible = false;
+
+  componentDidLoad() {
+    setTimeout(() => {
+      this.visible = true;
+    }, 1000);
+  }
+
   // https://codepen.io/adrianmg/pen/Omedpo
   render() {
     return (
-      <Host>
+      <Host class={{ show: this.visible }}>
         <div></div>
         <div></div>
         <div></div>
