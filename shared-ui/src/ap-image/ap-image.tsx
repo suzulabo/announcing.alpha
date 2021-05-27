48b27c3596570811234e8d6818734ce9a81d0a16
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-05-28 04:17:39 +0900

    refactor: remove image open  (ap-image

--- shared-ui/src/ap-image/ap-image.tsx
+++ shared-ui/src/ap-image/ap-image.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Host, Prop, State, Watch } from '@stencil/core';
+import { Component, h, Prop, State, Watch } from '@stencil/core';
 
 @Component({
   tag: 'ap-image',
@@ -7,9 +7,6 @@ import { Component, h, Host, Prop, State, Watch } from '@stencil/core';
 export class ApImage {
   @Prop()
   loader?: () => Promise<string>;
-
-  @Prop()
-  canOpen = true;
 
   @Watch('loader')
   watchLoader(loader: ApImage['loader']) {
@@ -37,63 +34,18 @@ export class ApImage {
   @State()
   loaderError = false;
 
-  @State()
-  open = false;
-
   componentWillLoad() {
     this.watchLoader(this.loader);
   }
 
-  private iamgeClick = () => {
-    if (!this.canOpen) {
-      return;
-    }
-    this.open = true;
-  };
-
-  private closeClick = () => {
-    this.open = false;
-  };
-
   render() {
     if (this.loaderError) {
-      return (
-        <Host class="error">
-          <ap-icon icon="exclamationDiamondFill" />
-        </Host>
-      );
+      return <ap-icon icon="exclamationDiamondFill" />;
     }
 
     if (this.src) {
-      if (!this.open) {
-        return (
-          <Host class="loaded">
-            <div class="wrapper">
-              <img
-                class={{ ['can-open']: this.canOpen }}
-                src={this.src}
-                onClick={this.iamgeClick}
-              />
-            </div>
-          </Host>
-        );
-      } else {
-        return (
-          <Host class="loaded">
-            <div class="wrapper open">
-              <pinch-zoom>
-                <img src={this.src} />
-              </pinch-zoom>
-              <ap-icon onClick={this.closeClick} icon="xCircle" />
-            </div>
-          </Host>
-        );
-      }
+      return <img src={this.src} />;
     }
-    return (
-      <Host class="loading">
-        <ap-icon icon="image" />
-      </Host>
-    );
+    return <ap-spinner />;
   }
 }
