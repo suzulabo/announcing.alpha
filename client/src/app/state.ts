6da44848f37cd27e212701b1f3986287bf27d54e
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-05-28 11:53:32 +0900

    fix: should check value is same at updating announce state

--- client/src/app/state.ts
+++ client/src/app/state.ts
@@ -6,6 +6,22 @@ const store = createStore<{ [k: string]: unknown }>({});
 export class AppState {
   announce = {
     set: (id: string, v: DataResult<AnnounceState>) => {
+      // check same value or not
+      const cur = this.announce.get(id);
+      b: {
+        if (
+          cur?.state == 'SUCCESS' &&
+          v.state == 'SUCCESS' &&
+          cur.value.uT.toMillis() != v.value.uT.toMillis()
+        ) {
+          break b;
+        }
+        if (cur?.state != v.state) {
+          break b;
+        }
+        return;
+      }
+
       store.set(`announce.${id}`, v);
     },
     get: (id: string) => {
