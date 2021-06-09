65817eb87e9f1b72c33dedd2a2550f4a3f948332
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-06-09 17:57:46 +0900

    ui3-console: setup app-root

--- console/src/components/app-root/app-root.tsx
+++ console/src/components/app-root/app-root.tsx
@@ -1,14 +1,51 @@
-import { Component, h, Host, Listen, State } from '@stencil/core';
+import { Component, h, Host } from '@stencil/core';
 import { App } from 'src/app/app';
 import { AppFirebase } from 'src/app/firebase';
 import { AppMsg } from 'src/app/msg';
 import { AppState } from 'src/app/state';
 import { AppEnv } from 'src/shared';
+import { RouteMatch } from 'src/shared-ui/ap-root/ap-root';
 
-interface MatchPathResult {
-  tag: string;
-  params?: { [k: string]: string };
-}
+const matches: RouteMatch[] = [
+  {
+    pattern: '',
+    tag: 'app-home',
+  },
+  {
+    pattern: 'signin',
+    tag: 'app-signin',
+  },
+  {
+    pattern: 'create',
+    tag: 'app-announce-create',
+  },
+  {
+    pattern: /^[0-9A-Z]{12}$/,
+    name: 'announceID',
+    tag: 'app-announce',
+    nexts: [
+      {
+        pattern: 'edit',
+        tag: 'app-announce-edit',
+      },
+      {
+        pattern: 'post',
+        tag: 'app-post-form',
+      },
+      {
+        pattern: /^[0-9a-zA-Z]{8}$/,
+        name: 'postID',
+        tag: 'app-post',
+        nexts: [
+          {
+            pattern: 'edit',
+            tag: 'app-post-form',
+          },
+        ],
+      },
+    ],
+  },
+];
 
 @Component({
   tag: 'app-root',
@@ -17,22 +54,7 @@ interface MatchPathResult {
 export class AppRoot {
   private app: App;
 
-  @State()
-  path: string;
-
-  @Listen('popstate', { target: 'window' })
-  handlePopState() {
-    const r = this.checkRedirect();
-    if (r) {
-      this.app.redirectRoute(r);
-      return;
-    }
-
-    const p = location.pathname;
-    this.path = p;
-  }
-
-  async componentWillLoad() {
+  constructor() {
     const appMsg = new AppMsg();
     document.querySelector('ap-error').msgs = appMsg.msgs.error;
 
@@ -40,15 +62,13 @@ export class AppRoot {
     const appState = new AppState();
     const appFirebase = new AppFirebase(appEnv, appState, appMsg);
     this.app = new App(appEnv, appMsg, appFirebase, appState);
-    await this.app.processLoading(() => {
-      return this.app.init();
-    });
-    this.handlePopState();
   }
 
-  private checkRedirect() {
-    const p = location.pathname;
+  async componentWillLoad() {
+    await this.app.init();
+  }
 
+  private handleRedirect = (p: string) => {
     if (this.app.isSignIn) {
       if (p == '/signin') {
         return '/';
@@ -58,83 +78,16 @@ export class AppRoot {
         return '/signin';
       }
     }
-
-    if (!this.getRoute()) {
-      return '/';
-    }
-  }
-
-  private staticRouteMap = new Map([
-    ['/signin', 'app-signin'],
-    ['/create', 'app-announce-create'],
-    ['/', 'app-home'],
-  ]);
-  private announceIDPattern = /^[A-Z0-9]{12}$/;
-  private postIDPattern = /^[-a-zA-Z0-9]+$/;
-
-  private getRoute(): MatchPathResult {
-    const p = location.pathname;
-
-    {
-      const t = this.staticRouteMap.get(p);
-      if (t) {
-        return { tag: t };
-      }
-    }
-
-    b: {
-      const l = p.split('/');
-      l.shift();
-      if (l.length > 3) {
-        break b;
-      }
-
-      const [announceID, postID, postEdit] = l;
-      if (!this.announceIDPattern.test(announceID)) {
-        break b;
-      }
-
-      if (postEdit) {
-        if (postEdit == 'edit_') {
-          return { tag: 'app-post-form', params: { announceID, postID } };
-        }
-        break b;
-      }
-
-      if (postID) {
-        if (postID == 'edit_') {
-          return { tag: 'app-announce-edit', params: { announceID } };
-        }
-        if (postID == 'post_') {
-          return { tag: 'app-post-form', params: { announceID } };
-        }
-        if (this.postIDPattern.test(postID)) {
-          return { tag: 'app-post', params: { announceID, postID } };
-        }
-      } else {
-        return { tag: 'app-announce', params: { announceID } };
-      }
-    }
-  }
+  };
 
   render() {
-    const m = this.getRoute();
-
     return (
       <Host>
-        <m.tag class="page" app={this.app} {...m.params} />
-        <footer>
-          <div class="title">
-            <a {...this.app.href('/')}>{this.app.msgs.footer.title}</a>
-          </div>
-          <div class="build-info">Version: {this.app.buildInfo.src}</div>
-          <div class="build-info">Built at {new Date(this.app.buildInfo.time).toISOString()}</div>
-          <div class="github">
-            <a href="https://github.com/suzulabo/announcing">
-              <ap-icon icon="github" />
-            </a>
-          </div>
-        </footer>
+        <ap-root
+          routeMatches={matches}
+          redirect={this.handleRedirect}
+          componentProps={{ app: this.app }}
+        />
       </Host>
     );
   }
