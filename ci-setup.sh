c47cb3232004034d3942043ca14c7321f0a33ab3
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-07-08 21:01:49 +0900

    ci-debug

--- ci-setup.sh
+++ ci-setup.sh
@@ -3,3 +3,5 @@ echo "${_APPENV_TS}" > shared/src/appenv.env.ts
 echo '${_FIREBASERC}' > firebase/.firebaserc
 
 echo '${_ASSETLINKS_JSON}' > client/assetlinks.json
+
+echo '${_GOOGLE_SERVICES_JSON}' > client/google-services.json
