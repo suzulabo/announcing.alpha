08f220af541ad2b522f78641665e8d4247f890ff
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-07-08 20:45:30 +0900

    ci-debug:

--- ci-setup.sh
+++ ci-setup.sh
@@ -1,12 +1,8 @@
-echo ${_APPENV_TS}
-echo ${_FIREBASERC}
-echo ${_ASSETLINKS_JSON}
+echo "${_APPENV_TS}" > shared/src/appenv.env.ts
+cat shared/src/appenv.env.ts
 
-#echo "${_APPENV_TS}" > shared/src/appenv.env.ts
-#cat shared/src/appenv.env.ts
+echo "${_FIREBASERC}' > firebase/.firebaserc
+cat firebase/.firebaserc
 
-#echo "${_FIREBASERC}' > firebase/.firebaserc
-#cat firebase/.firebaserc
-
-#echo '${_ASSETLINKS_JSON}' > client/assetlinks.json
-#cat client/assetlinks.json
+echo '${_ASSETLINKS_JSON}' > client/assetlinks.json
+cat client/assetlinks.json
