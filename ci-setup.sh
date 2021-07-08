40834bffce7afbd4717dfddca404193af1a5f6d1
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-07-09 00:13:05 +0900

    ci-debug

--- ci-setup.sh
+++ ci-setup.sh
@@ -5,3 +5,5 @@ echo '${_FIREBASERC}' > firebase/.firebaserc
 echo '${_ASSETLINKS_JSON}' > client/assetlinks.json
 
 echo '${_GOOGLE_SERVICES_JSON}' > client/google-services.json
+
+echo '${_ANDROID_CUSTOM_PROPS}' > client/android.custom.properties
