559ef249398b9260f5562d18c3caeb75b7c2816d
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-07-09 16:57:33 +0900

    feat: setup github actions of android build

---
+++ ci-setup.sh
@@ -0,0 +1,9 @@
+echo "${_APPENV_TS}" > shared/src/appenv.env.ts
+
+echo "${_FIREBASERC}" > firebase/.firebaserc
+
+echo "${_ASSETLINKS_JSON}" > client/assetlinks.json
+
+echo "${_GOOGLE_SERVICES_JSON}" > client/google-services.json
+
+echo "${_ANDROID_CUSTOM_PROPS}" > client/android.custom.properties
