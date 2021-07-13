3e6f6c5747cf875e6b13a7da0ab0085e15bb6c77
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-07-14 02:19:58 +0900

    fix: iOS app won't start

--- client/src/app/idbcache.ts
+++ client/src/app/idbcache.ts
@@ -1,27 +1,34 @@
 import { IDBPDatabase, openDB } from 'idb';
-import idbReady from 'safari-14-idb-fix';
 
 const DB_NAME = 'announcing-idb-cache';
 const STORE_NAME = 'cache';
 
 export class AppIdbCache {
-  private db!: IDBPDatabase;
+  private db?: IDBPDatabase;
 
   async init() {
-    await idbReady();
-
-    this.db = await openDB(DB_NAME, 1, {
+    // It's can hang in Safari
+    // https://bugs.webkit.org/show_bug.cgi?id=226547
+    openDB(DB_NAME, 1, {
       upgrade: db => {
         db.createObjectStore(STORE_NAME);
       },
+    }).then(db => {
+      this.db = db;
     });
   }
 
   async get<T>(k: string) {
-    return (await this.db.get(STORE_NAME, k)) as T;
+    if (this.db) {
+      return (await this.db.get(STORE_NAME, k)) as T;
+    }
+    return;
   }
 
   async set<T>(k: string, v: T) {
-    await this.db.put(STORE_NAME, v, k);
+    if (this.db) {
+      await this.db.put(STORE_NAME, v, k);
+    }
+    return;
   }
 }
