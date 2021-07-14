6c84176d025aa14b39e1e408d754bb5a08002e2a
suzulabo <41730006+suzulabo@users.noreply.github.com>
2021-07-14 19:38:16 +0900

    chore: change appId

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.announcing.client',
+  appId: 'app.announcing.client1',
   appName: 'Announcing♪',
   webDir: 'cap',
   bundledWebRuntime: false,
